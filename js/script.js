1eec6bdab788d17e35d7c26a2a7bec0b42dd1921
Rayyan1222 <156992065+Rayyan1222@users.noreply.github.com>
2024-04-02 11:09:55 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function init() {
     function showMeText() {
       var textbox = document.getElementById('entryinput');
       document.getElementById('textoutput').innerHTML = textbox.value;
-      alert("Rayyan Basathia: This is a test  " + textbox.value);
+      alert("Rayyan Basathia: This is a test " + textbox.value);
     }
   
     button.addEventListener('click', showMeText);
