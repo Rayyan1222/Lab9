60aeecdd030026d121f1b87c2c60cca3507248f6
Rayyan1222 <156992065+Rayyan1222@users.noreply.github.com>
2024-04-02 11:05:59 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function init() {
+    var button = document.getElementById('entrybutton');
+  
+    function showMeText() {
+      var textbox = document.getElementById('entryinput');
+      document.getElementById('textoutput').innerHTML = textbox.value;
+      alert("Rayyan Basathia: This is a test  " + textbox.value);
+    }
+  
+    button.addEventListener('click', showMeText);
+  }
+  
+  window.addEventListener('load', init);
